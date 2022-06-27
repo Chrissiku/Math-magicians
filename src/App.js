491f9f2951e63fb352ff434675dd2757de3478d5
chrissiku <chrissiku5@gmail.com>
2022-06-28 01:54:10 +0200

    Setup project environement

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Math magicians Calculator</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This Is a Boiler model of Math-magicians calculator project Using
+          React.Js
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
