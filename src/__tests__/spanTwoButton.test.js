eafdd6ed33c2863a89fa7b0d3c267cd8138e34da
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-07 10:56:31 +0100

    Add Display, function and operation test

--- src/__tests__/spanTwoButton.test.js
+++ src/__tests__/spanTwoButton.test.js
@@ -2,8 +2,10 @@ import renderer from 'react-test-renderer';
 import SpanTwo from '../components/component_mocks/DigitButton';
 
 const props = { name: 'Chris', e: jest.fn() };
-test('Test Digit Button component', () => {
-  const TREE = renderer.create(<SpanTwo e={props.e} name={props.name} />).toJSON();
+test('Test Span two Button component', () => {
+  const TREE = renderer
+    .create(<SpanTwo e={props.e} name={props.name} />)
+    .toJSON();
 
   expect(TREE).toMatchSnapshot();
 });
