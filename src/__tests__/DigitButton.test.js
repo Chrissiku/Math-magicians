f06a3749124417295d3917b0eb2dd08ad7e22c2f
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-07 16:24:52 +0530

    Add user interaction with Digit and Function buttons

--- src/__tests__/DigitButton.test.js
+++ src/__tests__/DigitButton.test.js
@@ -1,9 +1,21 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { render, screen, fireEvent, getByText } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import DigitButton from '../components/component_mocks/DigitButton';
 
 const props = { name: 'Chris', e: jest.fn() };
-test('Test Digit Button component', () => {
-  const TREE = renderer.create(<DigitButton {...props} />).toJSON();
-  expect(TREE).toMatchSnapshot();
+
+describe("Digit Button testing", () => {
+  test('Test Digit Button component', () => {
+    const TREE = renderer.create(<DigitButton {...props} />).toJSON();
+    expect(TREE).toMatchSnapshot();
+  });
+
+  it("Test click event", () => {
+    render(<DigitButton {...props} />);
+    fireEvent.click(screen.getByText(props.name));
+    expect(props.e).toHaveBeenCalledTimes(1);
+  });
+
 });
+
