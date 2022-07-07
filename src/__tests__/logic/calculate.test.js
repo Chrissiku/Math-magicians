eabd3a33722fed7d44675e6827662d05805b637a
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-07 19:04:09 +0530

    Calculate js test

---
+++ src/__tests__/logic/calculate.test.js
@@ -0,0 +1,18 @@
+import calculate from "../../logic/calculate";
+
+
+it("Test Calculate JS", () => {
+    // Arrange
+    const OBJ = { total: 0, next: null, operation: null };
+
+    // ACT
+    let result = calculate(OBJ, '9');
+    result = calculate(result, '+');
+    result = calculate(result, '7');
+    result = calculate(result, '=');
+    
+
+    // Assert
+    expect(+result.total).toBe(16);
+
+});
